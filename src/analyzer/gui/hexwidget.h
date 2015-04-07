27af0b7b7619bff01b98ff6b5887121f013d76f5
Michał Budzyński <m.budzynski@samsung.com>
2015-04-07 06:40:13 +0200

    [gui] Initial HexWidget handling and drawing logic + Changed base class to QAbstractScrollArea + Added context menu for selection between HEX and BitField display + Ctrl+Mouse_Wheel font scaling + Basic Drawing operations for HEX and BIN modes

--- src/analyzer/gui/hexwidget.h
+++ src/analyzer/gui/hexwidget.h
@@ -1,18 +1,24 @@
 #ifndef HEXWIDGET_H
 #define HEXWIDGET_H
 
-#include <QScrollArea>
+#include <QAbstractScrollArea>
+#include <QMenu>
 #include <QByteArray>
 #include <QColor>
 
-class HexWidget : public QScrollArea
+class HexWidget : public QAbstractScrollArea
 {
     Q_OBJECT
 public:
+    // How should we represent the binary data
+    enum class DisplayFormat { BIT_FIELD, HEX};
+
     explicit HexWidget(QWidget *parent = 0);
 
     void setData(QByteArray data);
     void setHighlightColor(QColor color);
+    // set smaller or bigger font
+    void resizeFont(int sizeIncrement);
 
 signals:
     void selectionChanged(quint32 offset);
@@ -20,15 +26,40 @@ signals:
 public slots:
     // ask for highlighting on data region
     void highlight(quint32 offset, quint32 len);
+
+private slots:
     // select if the view should be in Hex or Binary
     void setRawDataDisplayFormat(QAction *action);
 
 private:
+    // show our context menu with display format options
+    void contextMenuEvent(QContextMenuEvent *event);
+    // draw the data contents of the widget
+    void paintEvent(QPaintEvent *event);
+    void wheelEvent(QWheelEvent *event);
+    void resizeEvent(QResizeEvent *event);
+
+    // calculate column widths, line heights, etc.
+    void recalculateFontMetrics();
+
     QByteArray m_data;
     QColor m_highlightColor;
-    QWidget* m_offsetArea;
-    QWidget* m_rawDataArea;
-    QWidget* m_AsciiArea;
+    QMenu m_contextMenu;
+    DisplayFormat m_format;
+
+    int m_numBytesInLine;
+    int m_numAddressChars;
+    int m_textMargin;
+    int m_characterWidth;
+    int m_addrColumnWidth;
+    int m_rawColumnWidth;
+    int m_printableColumnWidth;
+    int m_lineWidth;
+    int m_lineHeight;
+    int m_numLines;
+    int m_numVisibleLines;
 };
+
+Q_DECLARE_METATYPE(HexWidget::DisplayFormat);
 
 #endif // HEXWIDGET_H
